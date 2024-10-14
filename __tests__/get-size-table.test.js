1618c3463741f9d1fd2f3a0e0b7a9360c7d8ce9e
Yong Lee <77824603+YongLeeCode@users.noreply.github.com>
2024-10-14 09:38:38 +0900

    Update get-size-table.test.js

--- __tests__/get-size-table.test.js
+++ __tests__/get-size-table.test.js
@@ -49,4 +49,7 @@ describe("getSizeTable", () => {
     expect(consoleSpy).toHaveBeenCalledWith("Fail to create Table");
     consoleSpy.mockRestore();
   });
+  it("success", () => {
+      expect(true).toBe(true);
+  });
 });
